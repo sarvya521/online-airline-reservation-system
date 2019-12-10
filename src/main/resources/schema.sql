6fbcc8e7e45b0b351189fc48cd1a01742b0d0449
Sarvesh Padwal <sarveshp@unifytech.com>
2019-12-11 02:22:00 +0530

    final schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,3 +20,56 @@ CREATE TABLE IF NOT EXISTS user_role (
   CONSTRAINT fk_user_role_app_user FOREIGN KEY(fk_user_id) REFERENCES app_user(id),
   CONSTRAINT fk_user_role_app_role FOREIGN KEY(fk_role_id) REFERENCES app_role(id)
 );
+
+CREATE TABLE IF NOT EXISTS airport (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  alias VARCHAR(10) NOT NULL,
+  CONSTRAINT uk_airport_name UNIQUE(name),
+  CONSTRAINT uk_airport_alias UNIQUE(alias),
+);
+
+CREATE TABLE IF NOT EXISTS aircraft (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  code VARCHAR(100) NOT NULL,
+  model VARCHAR(10) NOT NULL,
+  total_business_seats INT,
+  total_firstclass_seats INT,
+  total_economy_seats INT NOT NULL,
+  CONSTRAINT uk_aircraft_code UNIQUE(code)
+);
+
+CREATE TABLE IF NOT EXISTS flight (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  travel_date DATE NOT NULL,
+  departure_time TIME NOT NULL,
+  arrival_time TIME NOT NULL,
+  departure_from BIGINT NOT NULL,
+  arrival_at BIGINT NOT NULL,
+  business_class_fare INT,
+  firstclass_fare INT,
+  economy_class_fare INT NOT NULL,
+  remaining_business_seats INT,
+  remaining_firstclass_seats INT,
+  remaining_economy_seats INT,
+  airline VARCHAR(50) NOT NULL,
+  fk_aircraft_id BIGINT NOT NULL,
+  CONSTRAINT fk_flight_departure_airport FOREIGN KEY(departure_from) REFERENCES airport(id),
+  CONSTRAINT fk_flight_arrival_airport FOREIGN KEY(arrival_at) REFERENCES airport(id),
+  CONSTRAINT fk_flight_arrival_aircraft FOREIGN KEY(fk_aircraft_id) REFERENCES aircraft(id)
+);
+
+CREATE TABLE IF NOT EXISTS booking (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  pnr VARCHAR(10) NOT NULL,
+  booking_date DATE NOT NULL,
+  travel_date DATE NOT NULL,
+  seat_class VARCHAR(10) NOT NULL,
+  cost INT NOT NULL,
+  fk_flight_id BIGINT NOT NULL,
+  fk_user_id BIGINT NOT NULL,
+  CONSTRAINT fk_booking_flight FOREIGN KEY(fk_flight_id) REFERENCES flight(id),
+  CONSTRAINT fk_booking_app_user FOREIGN KEY(fk_user_id) REFERENCES app_user(id)
+);
+
+
